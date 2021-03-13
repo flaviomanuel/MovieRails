e7dc495a871c0d30ff4efa0495338bcd2abbe01c
flaviomanuel <flaviomanuel@gmail.com>
2021-03-13 19:25:40 -0300

    list all datas from api in HomePage

--- web/src/pages/HomePage/index.js
+++ web/src/pages/HomePage/index.js
@@ -1,90 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../../components/Header';
 
 import Search from '../../components/Search';
-
 import Card from '../../components/Card';
+
+import api from '../../services/api';
 
 
 function HomePage() {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect( () => {
+    async function fetchData(){
+      const response = await api.get("movie")
+            setMovies(response.data.data)
+            
+    }
+     fetchData()
+    },[])
+
     return (
         <>
             <Header link="/create-movie"/>
             <Search/>
             <main className="w-2/3 container mx-auto grid lg:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4  gap-16  mb-10">
-                <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                 <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-
-                <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                 <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                                <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                 <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                                <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                 <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
-                <Card
-                    name="Blade Nanner"
-                    release_date="29/02/1992"
-                    director="Ridley Scott"
-                    score="4"
-                />
+                {movies.map( movie => {
+                    return (
+                    <Card
+                        key={movie.id}
+                        name={movie.name}
+                        release_date={movie.release_date}
+                        director={movie.director}
+                        score={movie.score}
+                    />
+                    )
+                })}
+              
             </main>
         </>
     )
