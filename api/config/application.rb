335a6663da598451df1237f6ff422a6195e1fa5a
flaviomanuel <flaviomanuel@gmail.com>
2021-03-13 19:24:31 -0300

    active cors in api

--- api/config/application.rb
+++ api/config/application.rb
@@ -36,5 +36,13 @@ module MovieRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+   config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000'
+    
+        resource '*', headers: :any, credentials: :false, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
