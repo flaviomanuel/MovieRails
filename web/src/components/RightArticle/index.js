f58bb6d4f1604d10e13f36478f211b8b1be0c4b2
flaviomanuel <flaviomanuel@gmail.com>
2021-03-13 15:17:32 -0300

    allocating LeftArticle and RightArticle in CreatePage

---
+++ web/src/components/RightArticle/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Forms from '../Forms';
+
+function RightArticle(){
+    return (
+        <div className="container m-8 bg-white rounded-2xl">
+            <h1 className="font-bold text-titleForm mx-auto text-center m-4">Cadastrar informações sobre o filme</h1>
+            <Forms/> 
+        </div>
+    )
+}
+
+export default RightArticle
--- web/src/pages/CreatePage/index.js
+++ web/src/pages/CreatePage/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Forms from '../../components/Forms';
+
 import LeftArticle from '../../components/LeftArticle';
+import RightArticle from '../../components/RightArticle';
 
 function CreatePage(){
     return(
         <main className="flex">
-            <LeftArticle />
-            <div className="container m-16 bg-white rounded-2xl">
-                 <h1 className="font-bold text-titleForm mx-auto text-center m-4">Cadastrar informações sobre o filme</h1>
-                <Forms/> 
-            </div>
-        </main>
+        <LeftArticle/>
+        <RightArticle/>
+         </main>
     )
 }
 
