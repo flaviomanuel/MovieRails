739f545f8ee78234184ef72265501b48d1f1fa57
flaviomanuel <flaviomanuel@gmail.com>
2021-03-14 15:45:26 -0300

    onClick all logos redirect to HomePage

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import movieRailsLogo from '../../assets/images/movieRailsLogo.png'
 import Button from '../Button';
@@ -7,7 +8,9 @@ function Header({link}) {
     return (
         <header className="w-screen:container mx-auto bg-white py-4" >
             <div className="container sm:mx-auto flex content- justify-between items-center px-24">
+                <Link to="/">
                 <img className="h-16" src={movieRailsLogo} alt="movieRails logo"/>
+                </Link>
                 <Button link={link} text="Novo Filme"/>
             </div>
         </header>
--- web/src/components/LeftArticle/index.js
+++ web/src/components/LeftArticle/index.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import whiteMovieRails from '../../assets/images/whiteMovieRailsLogo.png'
 
 function LeftArticle({title}){
     return (
 
             <div className="bg-leftBackgroundRedRails h-screen w-2/5 flex flex-col justify-center items-center p-4 " >
-                
-                    <img src={whiteMovieRails} alt="logo movieRails branco" className="w-96 m-10"/>
+                    <Link className="w-90 m-4" to="/">
+                         <img src={whiteMovieRails} alt="logo movieRails branco" />
+                    </Link>
+
+                        {/* <img src={whiteMovieRails} alt="logo movieRails branco" className="w-96 m-10"/> */}
+
                     <h1 className="text-titleLeftMain text-white font-bold text-center max-w-xs">{title}</h1>
-                
-               
+
             </div>
            
     )
